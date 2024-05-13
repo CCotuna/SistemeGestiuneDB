01154de812ec9feb919d47c89ecabfbd441da1f7
CCotuna <126149136+CCotuna@users.noreply.github.com>
2024-05-14 01:46:07 +0300

    Proiect Carti De Vizita (Badges) - preluare date subiecti din DB
    Proiect 6:
    - Vue 3
    - Tailwind CSS - styling
    - Firebase - database

---
+++ VueBadges/src/views/EmployeeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex">
+        <div class="m-10">
+            <EmployeesList />
+        </div>
+    </div>
+
+</template>
+<script setup>
+import EmployeesList from '@/components/EmployeesList.vue';
+</script>
